aabef1e9cca28a3ba9dfb2c48ea94078375b2b24
Marcel Visser <marcel1337@hotmail.com>
2016-12-09 00:22:43 +0100

    Bootstrapped Angular

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Marcel on 9-12-2016.
+ */
+var statsApp = angular.module('statsApp', []);
+statsApp.controller('statsController', function($scope){
+    $scope.hello = 'it\'s me';
+})

dde9a296a19d1e38933176ea3d8b0f3d48f91dd6
Marcel Visser <marcel1337@hotmail.com>
2016-12-09 12:17:12 +0100

    Clean up

--- app.js
+++ app.js
@@ -2,20 +2,19 @@
  * Created by Marcel on 9-12-2016.
  */
 var statsApp = angular.module('statsApp', []);
+
+var d = (o) => +(new Date(parseInt(o[2]), parseInt(o[1]-1), parseInt(o[0])));
+var median = (o) => (o.length % 2 == 0)?(o[~~(o.length/2)]+o[~~(o.length/2 - 1)])/2:o[~~(o.length/2)];
+var prc = (o) => Math.round(o*1000)/10 + '%';
+var simpleDateRegex = /[0-3][0-9]-[01][0-9]-[[0-9]{4}/;
+
 statsApp.controller('statsController', function($scope){
-    $scope.hello = 'it\'s me';
-    var simpleDateRegex = /[0-3][0-9]-[01][0-9]-[[0-9]{4}/;
     $.ajax({
         method:'GET',
         url: 'woningnet.php',
     }).done(function(data){
-        var d = (o) => +(new Date(parseInt(o[2]), parseInt(o[1]-1), parseInt(o[0])));
-        var median = (o) => (o.length % 2 == 0)?(o[~~(o.length/2)]+o[~~(o.length/2 - 1)])/2:o[~~(o.length/2)];
-        var prc = (o) => Math.round(o*1000)/10 + '%';
-        data = JSON.parse(data);
-        data = data["Verantwoordingen"];
+        data = JSON.parse(data)["Verantwoordingen"];
         $scope.total = data.length;
-        //console.log(data);
         data = data.map(o => {
             //console.log(o);
             if(o.Omschrijving != null) {
@@ -23,18 +22,13 @@ statsApp.controller('statsController', function($scope){
                 if (date) {
                     return date[0].split('-');
                 }
-
             }
             return "REMOVE";
-        });
-
-        data = data.filter(o=>o!="REMOVE").sort((a,b) => {
+        }).filter(o=>o!="REMOVE").sort((a,b) => {
             return d(a) - d(b);
         });
-        $scope.nodate = $scope.total - data.length;
-        $scope.avg = data.reduce((p,n)=> [p[0] + +n[0], p[1] + +n[1], p[2] + +n[2]], [0,0,0]).map(o => Math.round(o/data.length));
-        $scope.median = median(data);
-        $scope.yearly = {}
+
+        $scope.yearly = {};
         data.forEach(o => {
             var yGroup = Math.floor(o[2]/5)*5;
             if(!$scope.yearly[yGroup]){
@@ -46,20 +40,21 @@ statsApp.controller('statsController', function($scope){
         for(key in $scope.yearly){
             $scope.yearly[key].perc = prc($scope.yearly[key].count / data.length);
         }
-        
+
+        $scope.nodate = $scope.total - data.length;
+        $scope.avg = data.reduce((p,n)=> [p[0] + +n[0], p[1] + +n[1], p[2] + +n[2]], [0,0,0]).map(o => Math.round(o/data.length));
+        $scope.median = median(data);
+
         $scope.$watch('cusDate', (n, o) => {
             if(n) {
                 console.log(n.match(simpleDateRegex));
                 if (n.match(simpleDateRegex)) {
-                    //console.log(data.length, data.filter((a)=>d(a) > d(n.split('-'))).length);
                     $scope.percentile = prc(1 - data.filter((a)=>d(a) <= d(n.split('-'))).length/data.length);
                 }
             }else{
                 $scope.percentile = 'N/A';
             }
-        })
-
-
+        });
 
         $scope.$apply();
     });
--- woningnet.php
+++ woningnet.php
@@ -4,5 +4,8 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');
-    echo file_get_contents("https://www.woningnetregioutrecht.nl/webapi/verantwoording/verantwoording/?Periode=4&DirectBemiddeling=false&Plaatsen%5B%5D=177&Plaatsen%5B%5D=176&Plaatsen%5B%5D=340&Plaatsen%5B%5D=460&Plaatsen%5B%5D=782&Plaatsen%5B%5D=1162&Plaatsen%5B%5D=1754&Plaatsen%5B%5D=1794&Plaatsen%5B%5D=2034&Soorten%5B%5D=G&Soorten%5B%5D=J&Soorten%5B%5D=HT&Soorten%5B%5D=N&Paging%5BCurrentPage%5D=");
+    $url = "https://www.woningnetregioutrecht.nl/webapi/verantwoording/verantwoording/?";
+    $params = "Periode=4&DirectBemiddeling=false&Plaatsen%5B%5D=177&Plaatsen%5B%5D=176&Plaatsen%5B%5D=340&Plaatsen%5B%5D=460&Plaatsen%5B%5D=782&Plaatsen%5B%5D=1162&Plaatsen%5B%5D=1754&Plaatsen%5B%5D=1794&Plaatsen%5B%5D=2034&Soorten%5B%5D=G&Soorten%5B%5D=J&Soorten%5B%5D=HT&Soorten%5B%5D=N&Paging%5BCurrentPage%5D=";
+    //Get this specific URL (with empty currentPage) so it barfs up all the data for my specific needs
+    echo file_get_contents($url.$params);
 ?>

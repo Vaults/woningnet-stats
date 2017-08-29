ee9a695004655ddfcd2d512cae88e5742848c1f3
Marcel Visser <marcel1337@hotmail.com>
2017-08-29 18:09:27 +0200

    Cleaned project

--- app.js
+++ app.js
@@ -1,61 +1,69 @@
 /**
  * Created by Marcel on 9-12-2016.
  */
-var statsApp = angular.module('statsApp', []);
+const statsApp = angular.module('statsApp', []);
 
-var d = (o) => +(new Date(parseInt(o[2]), parseInt(o[1]-1), parseInt(o[0])));
-var median = (o) => (o.length % 2 == 0)?(o[~~(o.length/2)]+o[~~(o.length/2 - 1)])/2:o[~~(o.length/2)];
-var prc = (o) => Math.round(o*1000)/10 + '%';
-var simpleDateRegex = /[0-3][0-9]-[01][0-9]-[[0-9]{4}/;
+const arrayToDate = (o) => +(new Date(parseInt(o[2]), parseInt(o[1] - 1), parseInt(o[0])));
+//Accuracy is not important for this application's purposes, we take the lazy way out so no averaging dates
+const lazyMedian = (o) => o[~~(o.length / 2)];
+const percentage = (o) => Math.round(o * 1000) / 10 + '%';
+const simpleDateRegex = /[0-3][0-9]-[01][0-9]-[[0-9]{4}/;
 
-statsApp.controller('statsController', function($scope){
+statsApp.controller('statsController', function ($scope) {
+
+    $scope.yearGroupSize = 4;
+
     $.ajax({
-        method:'GET',
+        method: 'GET',
         url: 'woningnet.php',
-    }).done(function(data){
+    }).done(function (data) {
+        //See if we have valid data
         data = JSON.parse(data)["Verantwoordingen"];
         $scope.total = data.length;
+        //Map all data by dates, only thing we're interested in now.
         data = data.map(o => {
-            //console.log(o);
-            if(o.Omschrijving != null) {
-                var date = o.Omschrijving.match(simpleDateRegex);
+            const potentialDate = o.Omschrijving;
+            if (potentialDate !== null) {
+                const date = potentialDate.match(simpleDateRegex);
                 if (date) {
                     return date[0].split('-');
                 }
             }
             return "REMOVE";
-        }).filter(o=>o!="REMOVE").sort((a,b) => {
-            return d(a) - d(b);
-        });
-
+        })
+            .filter(o => o != "REMOVE")
+            .sort((a, b) => {
+                return arrayToDate(a) - arrayToDate(b);
+            });
+        //Add years to year-groups
         $scope.yearly = {};
         data.forEach(o => {
-            var yGroup = Math.floor(o[2]/5)*5;
-            if(!$scope.yearly[yGroup]){
-                $scope.yearly[yGroup] = {count:1};
-            }else{
-                $scope.yearly[yGroup].count++;
+            const yearGroup = Math.floor(o[2] / $scope.yearGroupSize) * $scope.yearGroupSize;
+            if (!$scope.yearly[yearGroup]) {
+                $scope.yearly[yearGroup] = {count: 0};
             }
+            $scope.yearly[yearGroup].count++;
         });
-        for(key in $scope.yearly){
-            $scope.yearly[key].perc = prc($scope.yearly[key].count / data.length);
-        }
+        //Calculate percentage
+        Object.keys($scope.yearly).forEach(key => {
+            $scope.yearly[key].perc = percentage($scope.yearly[key].count / data.length);
+        });
 
+        //Calculate metadata
         $scope.nodate = $scope.total - data.length;
-        $scope.avg = data.reduce((p,n)=> [p[0] + +n[0], p[1] + +n[1], p[2] + +n[2]], [0,0,0]).map(o => Math.round(o/data.length));
-        $scope.median = median(data);
+        $scope.avg = data.reduce((p, n) => [p[0] + +n[0], p[1] + +n[1], p[2] + +n[2]], [0, 0, 0]).map(o => Math.round(o / data.length));
+        $scope.median = lazyMedian(data);
+        console.log($scope.median);
 
-        $scope.$watch('cusDate', (n, o) => {
-            if(n) {
-                console.log(n.match(simpleDateRegex));
-                if (n.match(simpleDateRegex)) {
-                    $scope.percentile = prc(1 - data.filter((a)=>d(a) <= d(n.split('-'))).length/data.length);
-                }
-            }else{
+        //Check for manual date input updates
+        $scope.$watch('cusDate', (n, _) => {
+            if (n && n.match(simpleDateRegex)) {
+                $scope.percentile = percentage(1 - data.filter((a) => arrayToDate(a) <= arrayToDate(n.split('-'))).length / data.length);
+            } else {
                 $scope.percentile = 'N/A';
             }
         });
-
+        //Pass to angular, we're working outside of its scope
         $scope.$apply();
     });
-})
+});
